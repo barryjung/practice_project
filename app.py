a11c2aec3aebff7ac453163108942b3d984d5b1d
barryjung <wjdgusrbs5@gmail.com>
2023-03-23 12:52:18 +0900

    add modify function
    add modify function to index.html and app.py

--- app.py
+++ app.py
@@ -35,5 +35,24 @@ def delete_task():
     return jsonify({'result': '삭제 완료!'})
 
 
+@app.route("/modify_set", methods=["POST"])
+def modify_set():
+    name = request.form['name_give']
+
+    task = db.tasks.find_one({'name': name}, {'_id': False})
+    return jsonify({'result': task})
+
+
+@app.route("/modify_confirm", methods=["POST"])
+def modify_confirm():
+    base_name = request.form['base_name_give']
+    name = request.form['name_give']
+    content = request.form['content_give']
+
+    doc = {'name': name, 'content': content}
+    db.tasks.update_one({'name': base_name}, {'$set': (doc)})
+    return jsonify({'result': '수정 완료!'})
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
